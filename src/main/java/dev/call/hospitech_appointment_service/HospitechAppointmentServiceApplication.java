53d1ac93941fb75683b16f77b18e58f9eee55238
armanoalves <armanobarrosalvesh@gmail.com>
2025-09-25 17:08:26 -0300

    first commit

---
+++ src/main/java/dev/call/hospitech_appointment_service/HospitechAppointmentServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.call.hospitech_appointment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitechAppointmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitechAppointmentServiceApplication.class, args);
+	}
+
+}
